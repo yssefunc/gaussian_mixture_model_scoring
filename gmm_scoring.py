980f35006448a8117f83cbbce7a6975c034dd925
YAVUZ SELIM SEFUNC <yssefunc@gmail.com>
2020-01-28 11:19:05 +0300

    Update gmm_scoring.py

--- gmm_scoring.py
+++ gmm_scoring.py
@@ -86,12 +86,11 @@ def gmm_customer_scoring(data):
     label_one = gmm_score.query("labels == 1").sort_values("label_one").reset_index(drop=True)
     label_one = label_one.drop(columns=['label_zero','label_two','label_three','label_four'])
     label_one=label_one.rename(columns = {'label_one':'score'})
-
+	
     #label_two
     label_two = gmm_score.query("labels == 2").sort_values("label_two").reset_index(drop=True)
     label_two = label_two.drop(columns=['label_zero','label_one','label_three','label_four'])
     label_two=label_two.rename(columns = {'label_two':'score'})
-
 
     #label_three
     label_three = gmm_score.query("labels == 3").sort_values("label_three").reset_index(drop=True)
@@ -106,8 +105,6 @@ def gmm_customer_scoring(data):
     #merging labels
     frames = [label_zero,label_one,label_two,label_three,label_four]
     vertical_stack = pd.concat(frames, axis=0).reset_index(drop=True)
-    
-
     
     #MinMax Scaling for scoring
     vertical_stack_scaling = vertical_stack[['score']]
@@ -129,3 +126,64 @@ def gmm_customer_scoring(data):
     result = result[['customer_id','recency','frequency','monetary','monetary_mean','monetary_min','monetary_max','monetary_std','labels','score']]
     
     return result
+
+	
+def scoring_plot(result):
+	# Add Scoring Groups to Plot
+	import matplotlib.pyplot as plt
+	import numpy as np
+
+	plt.figure(figsize=(16,8))
+
+	plt.hist(result['score'],
+			 bins=20,
+			 edgecolor='white',
+			 color = '#317DC2',
+			 linewidth=1.2)
+
+	plt.xlim(0,1)
+	plt.title('Scoring Groups', fontweight="bold", fontsize=14)
+	plt.xlabel('Score')
+	plt.ylabel('Count')
+
+	# Percentile Lines
+	plt.axvline(np.percentile(result['score'],25), color='red', linestyle='dashed', linewidth=2, alpha=0.6)
+	plt.axvline(np.percentile(result['score'],50), color='orange', linestyle='dashed', linewidth=2, alpha=0.6)
+	plt.axvline(np.percentile(result['score'],75), color='green', linestyle='dashed', linewidth=2, alpha=0.6)
+
+	# Text
+	plt.text(0.10, 80000, 'Excellent', color='red', fontweight='bold', style='italic', fontsize=12)
+	plt.text(0.50, 80000, 'Fair', color='green', fontweight='bold', style='italic', fontsize=12)
+	plt.text(0.9, 80000, 'Outlier', color='blue', fontweight='bold', style='italic', fontsize=12)
+
+	#Shading between Percentiles
+	plt.axvspan(0, 0.40, alpha=0.1, color='blue')
+	plt.axvspan(0.40, 0.70, alpha=0.1, color='yellow')
+	plt.axvspan(0.70, 1, alpha=0.1, color='red');
+
+def three_d_plot(data1,data2,data3,data4):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib
+    #fig = plt.figure()
+    fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(111, projection='3d')
+    colors = ['aqua', 'darkgray', 'blue', 'red', 'black', 'yellow', 'green']
+
+    
+    #ax.scatter(data1,data2,data3, color="black")
+    #ax.scatter(data4,data5,data6, color="red", s=150)
+    #ax.scatter(data1,data2,data3, s=100, edgecolor="r", facecolor="gold")
+    ax.scatter(data1,data2,data3, c=data4, cmap=matplotlib.colors.ListedColormap(colors))
+    #plt.legend(loc=2)
+    plt.title("GMM 3d plot")
+    ax.set_xlabel('Recency')
+    ax.set_ylabel('Frequency')
+    ax.set_zlabel('Monetary')
+    plt.show()
+	
+#three_d_plot(df_GMM["recency"],df_GMM["frequency"],df_GMM["monetary"],df_GMM["labels"])
+
+
+
+
